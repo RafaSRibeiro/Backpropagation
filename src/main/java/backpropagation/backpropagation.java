77e3a4bb7041e54bc4d8877545a0354e9536acc7
Rafael Ribeiro <rafael.s.ribeiro@sc.senai.br>
2019-09-12 19:35:39 -0300

    criado método de inicialização dos arrays

--- src/main/java/backpropagation/backpropagation.java
+++ src/main/java/backpropagation/backpropagation.java
@@ -2,11 +2,23 @@ package backpropagation;
 
 public class backpropagation {
 
+    private double v[][] = new double[7][9];
+    private double w[][] = new double[7][9];
+
+    private void initializeWeights() {
+        for (int i = 0; i < v.length; i++) {
+            for (int j = 0; j < v[i].length; i++) {
+                v[i][j] = Math.random();
+            }
+        }
+    }
+
     public static double sigmoideBinaria(double x) {
         return 1 / (1 + Math.exp(-x));
     }
 
     public static double sigmoideBipolar(double x) {
-        return (2 / (1 + Math.exp(-x)))*-1;
+        return (2 / (1 + Math.exp(-x))) * -1;
     }
+
 }
